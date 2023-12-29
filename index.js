e014133187fff8b45a131dec372b7aa18793e127
Rohit Singh <rohitssingh1610@gmail.com>
2023-12-29 22:38:09 +0530

    Whitelist client

--- index.js
+++ index.js
@@ -4,12 +4,27 @@ const { Server } = require("socket.io");
 const cors = require('cors');
 
 const app = express();
-app.use(cors({origin: 'http://localhost:3000'}))
+const isDev = app.settings.env === 'development'
+const URL = isDev ? 'http://localhost:3000' : 'https://sketchbook-three.vercel.app/'
+app.use(cors({origin: URL}))
 const httpServer = createServer(app);
-const io = new Server(httpServer, { cors: 'http://localhost:3000' });
+const io = new Server(httpServer, {cors: URL});
 
 io.on("connection", (socket) => {
   console.log("server working");
+
+  socket.on('beginPath', (arg) => {
+    socket.broadcast.emit('beginPath', arg)
+  })
+
+  socket.on('drawLine', (arg) => {
+    socket.broadcast.emit('drawLine', arg)
+  })
+
+  socket.on('changeConfig', (arg) => {
+    socket.broadcast.emit('changeConfig', arg)
+  })
+
 });
 
 httpServer.listen(5000);
